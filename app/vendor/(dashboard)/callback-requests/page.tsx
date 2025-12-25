515d3bd2ca0bae9149a5f06233de9e57ea9bd750
vijaypitani <vjvijay653@gmail.com>
2025-12-25 17:51:37 +0530

    vendor dashboard components structure commit

---
+++ app/vendor/(dashboard)/callback-requests/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CallbackRequest = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default CallbackRequest
---
+++ app/vendor/(dashboard)/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BusinessPortfolio = () => {
+  return (
+    <div>BusinessPortfolio</div>
+  )
+}
+
+export default BusinessPortfolio
